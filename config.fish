c501f2f8cac4feda96d63bf478f574f5bdb1387a
Austin Diviness <austin.diviness@gmail.com>
2013-09-24 08:05:14 +0000

    added fish shell config file

---
+++ config.fish
@@ -0,0 +1,20 @@
+
+# some aliases that seem nice
+alias ls "ls --color=auto"
+alias grep "grep --color=auto"
+alias fgrep "fgrep --color=auto"
+alias egrep "egrep --color=auto"
+alias ll "ls -alF"
+alias la "ls -A"
+
+# set term to be 256 colors
+set TERM xterm-256color
+
+# utility aliases for programs I use
+alias emacs "emacs -nw"
+
+alias u "cd .."
+alias uu "cd ../.."
+alias uuu "cd ../../.."
+alias uuuu "cd ../../../.."
+alias uuuuu "cd ../../../../.."
