23758d44015993eeb2ea38966424f0876895ec8f
Austin Diviness <austin.diviness@gmail.com>
2013-10-30 20:06:49 -0600

    created xmonad config specific to Eir, added xmonad stuff to xinitrc, created xmobar config

--- config.fish
+++ config.fish
@@ -28,6 +28,7 @@ alias rcs "vim ~/rcs/"
 alias asgard "ssh -o PORT=8081 austin@lightningcougar.dyndns.info"
 alias chromeos "sudo cgpt -i 6 -P 0 -S 1 /dev/sda"
 alias git-diff "git difftool --tool=vimdiff"
+alias wifi "sudo wifi-menu mlan0"
 
 # moving up dirs
 alias u "cd .."
